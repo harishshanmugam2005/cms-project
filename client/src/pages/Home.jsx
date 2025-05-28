534daac7e6ddb0f9b827e86723ea9d35e24fe039
Anstron1517 <anstronanstron327@gmail.com>
2025-05-28 15:01:51 +0530

    home intial

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,12 @@
+import React from "react";
 export default function Home() {
   return (
     <>
-      <section className="flex flex-col items-center justify-center min-h-[80vh] px-4">
-        <div className="">
-            HOME
-        </div>
+     <section className="flex flex-col items-center justify-center min-h-[80vh] px-4">
         <div className="text-gray-800 text-3xl font-bold mb-4">
           Welcome to My Application
         </div>
-        <button>
+        <button className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded-lg shadow-lg text-lg transition duration-300">
           Get Started
         </button>
       </section>
