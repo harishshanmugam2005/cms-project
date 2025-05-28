354c124822820f9cca20112f39e3b79a02b26acd
Anstron1517 <anstronanstron327@gmail.com>
2025-05-28 12:09:33 +0530

    change on home

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router";
+
+export default function Home() {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center min-h-[80vh] px-4">
+        <div className="">
+            HOME
+        </div>
+        <div className="text-gray-800 text-3xl font-bold mb-4">
+          Welcome to My Application
+        </div>
+        <button>
+          Get Started
+        </button>
+      </section>
+    </>
+  );
+}
