e20118681f3d36d22f410627a3144dfab99406d6
harishshanmugam2005 <harishshanmugam2005@gmail.com>
2025-05-30 09:04:52 +0000

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 import SignUp from './pages/Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <SignUp/>
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,8 @@ import tailwind from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    allowedHosts:["5173-harishshanmugam2005-cms-79tvwyr32n.app.codeanywhere.com"],
+  },
   plugins: [react(),tailwind()],
 })
